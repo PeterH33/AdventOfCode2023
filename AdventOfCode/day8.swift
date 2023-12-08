7ea413bf0efb6fb43fb7f89062865b66952b0a4c
Peter Hartnett <peter.hartnett@gmail.com>
2023-12-08 14:46:42 -0500

    Adding day8 stub and data

---
+++ AdventOfCode/day8.swift
@@ -0,0 +1,18 @@
+//
+//  day8.swift
+//  AdventOfCode
+//
+//  Created by Peter Hartnett on 12/8/23.
+//
+
+import SwiftUI
+
+struct day8View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    day8View()
+}
